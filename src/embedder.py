e2b50385e4b1e0689c9c09cc98a7795c39555e1d
ShukuNinja <sri2410089@ssn.edu.in>
2026-06-08 20:51:03 +0530

    Embedding pipeline functions written

---
+++ src/embedder.py
@@ -0,0 +1,45 @@
+import json
+import numpy as np
+from sentence_transformers import SentenceTransformer
+from src.constants import EMBEDDING_MODEL, EMBEDDING_BATCH_SIZE, NORMALIZE_EMBEDDINGS
+
+_model = None
+
+def load_chunk_data(chunk_file_path: str) -> dict:
+    with open(chunk_file_path, 'r', encoding = 'utf-8') as file:
+        chunk_data = json.load (file)
+    return chunk_data
+
+def extract_texts(chunk_data: dict) -> list[str]:
+    return [
+        chunk["text"] 
+        for chunk in chunk_data["chunks"]
+    ]
+
+def get_model():
+    global _model
+
+    if _model is None:
+        _model = SentenceTransformer(EMBEDDING_MODEL)
+    
+    return _model
+
+def generate_embeddings(texts: list[str], EMBEDDING_BATCH_SIZE) -> np.ndarray:
+    if not texts:
+        raise ValueError("No texts provided for embedding.")
+    
+    model = get_model()
+
+    embeddings = model.encode(
+        texts,
+        batch_size = EMBEDDING_BATCH_SIZE,
+        show_progress_bar = True,
+        normalize_embeddings = NORMALIZE_EMBEDDINGS,
+        convert_to_numpy = True
+    )
+
+    return embeddings.astype(np.float32)
+
+def save_embeddings(embeddings: np.ndarray, embeddings_file_path: str):
+    np.save(embeddings_file_path, embeddings)
+    return embeddings_file_path
